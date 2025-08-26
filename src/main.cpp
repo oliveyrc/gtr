ac3d55bcdf4286fdd4ee1ba34352bf4e0ecd9ef0
Richard Olivey <richard@olivey.net>
2025-08-26 21:47:38 +0100

    Adding lazyFoo demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,146 @@
 #include <iostream>
 
-#include "SDL3/SDL.h"
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main() {
+#include <string>
 
-    SDL_Log("%s", "Hi Mum");
-    //std::cout << "Hello, World!" << std::endl;
-    return 0;
+/* Constants */
+//Screen dimension constants
+constexpr int kScreenWidth{640};
+constexpr int kScreenHeight{480};
+
+
+/* Global Variables */
+//The window we'll be rendering to
+SDL_Window *gWindow{nullptr};
+
+//The surface contained by the window
+SDL_Surface *gScreenSurface{nullptr};
+
+//The image we will load and show on the screen
+SDL_Surface *gHelloWorld{nullptr};
+
+
+/* Function Prototypes */
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+bool init() {
+    //Initialization flag
+    bool success{true};
+
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) == false) {
+        SDL_Log("SDL could not initialize! SDL error: %s\n", SDL_GetError());
+        success = false;
+    } else {
+        //Create window
+        if (gWindow = SDL_CreateWindow("SDL3 Tutorial: Hello SDL3", kScreenWidth, kScreenHeight, 0);
+            gWindow == nullptr) {
+            SDL_Log("Window could not be created! SDL error: %s\n", SDL_GetError());
+            success = false;
+        } else {
+            //Get window surface
+            gScreenSurface = SDL_GetWindowSurface(gWindow);
+        }
+    }
+
+    return success;
+}
+
+void close() {
+    //Clean up surface
+    SDL_DestroySurface( gHelloWorld );
+    gHelloWorld = nullptr;
+
+    //Destroy window
+    SDL_DestroyWindow( gWindow );
+    gWindow = nullptr;
+    gScreenSurface = nullptr;
+
+    //Quit SDL subsystems
+    SDL_Quit();
+}
+bool loadMedia()
+{
+    //File loading flag
+    bool success{ true };
+
+    //Load splash image
+    std::string imagePath{ "assets/hello-sdl3.bmp" };
+    if( gHelloWorld = SDL_LoadBMP( imagePath.c_str() ); gHelloWorld == nullptr )
+    {
+        SDL_Log( "Unable to load image %s! SDL Error: %s\n", imagePath.c_str(), SDL_GetError() );
+        success = false;
+    }
+
+    return success;
+}
+
+
+int main( int argc, char* args[] )
+{
+    //Final exit code
+    int exitCode{ 0 };
+
+    //Initialize
+    if( init() == false )
+    {
+        SDL_Log( "Unable to initialize program!\n" );
+        exitCode = 1;
+    }
+    else
+    {
+        //Load media
+        if( loadMedia() == false )
+        {
+            SDL_Log( "Unable to load media!\n" );
+            exitCode = 2;
+        }
+        else
+        {
+            //The quit flag
+            bool quit{ false };
+
+            //The event data
+            SDL_Event e;
+            SDL_zero( e );
+
+            //The main loop
+            while( quit == false )
+            {
+                //Get event data
+                while( SDL_PollEvent( &e ) == true )
+                {
+                    //If event is quit type
+                    if( e.type == SDL_EVENT_QUIT )
+                    {
+                        //End the main loop
+                        quit = true;
+                    }
+                }
+
+                //Fill the surface white
+                SDL_FillSurfaceRect( gScreenSurface, nullptr, SDL_MapSurfaceRGB( gScreenSurface, 0xFF, 0xFF, 0xFF ) );
+
+                //Render image on screen
+                SDL_BlitSurface( gHelloWorld, nullptr, gScreenSurface, nullptr );
+
+                //Update the surface
+                SDL_UpdateWindowSurface( gWindow );
+            }
+        }
+    }
+
+    //Clean up
+    close();
+
+    return exitCode;
 }
